edfb63c1e81fa81327548ef467f03b2c0064f167
Julian Camilo Builes Serrano <buiser147@hotmail.com>
2021-09-25 19:03:02 -0500

    intento de hilos

--- Codigo/filtro.py
+++ Codigo/filtro.py
@@ -1,4 +1,7 @@
 import zmq
+import  threading
+from threading import Semaphore, Thread
+import time
 from Clases import  Oferta
 portEmp = "5000"
 portServ = "6000"
@@ -8,10 +11,72 @@ context = zmq.Context()
 socketSub =  context.socket(zmq.SUB)
 socketServer =  context.socket(zmq.REQ)
 
+socketServer.connect("tcp://{}:{}".format(host,portServ))
+
 socketSub.connect("tcp://{}:{}".format(host,portEmp))
 
+#semafor encargado para poder hacer uso de la lista de Ofertas enviadas por los Empleadores
+semaforo = Semaphore(1)
 
 socketSub.subscribe("")
+listOfertas = []
 
-ofer = socketSub.recv_pyobj()
-print(ofer)
+    
+class HiloEmpleador(Thread):
+    # este hilo se encarga de agregar cada oferta a 
+    # una lista de ofertas
+    def __init__(self,semaforo,Oferta): #Constructor de la clase
+         Thread.__init__(self)
+         self.semaforo = semaforo
+         self.Oferta = Oferta
+    def alamcenarOfertas(self):
+        listOfertas.append(self.Oferta)
+    def run(self): #Metodo que se ejecutara con la llamada start
+          self.semaforo.acquire()
+          self.alamcenarOfertas()
+          self.semaforo.release()
+    
+class HiloServidorEnviar(Thread):
+    # este hilo se encarga de verificar si hay la cantidad de ofertas 
+    # para ser enviadas al servidor
+    def __init__(self,semaforo): #Constructor de la clase
+         Thread.__init__(self)
+         self.semaforo = semaforo
+
+    def enviarOFertas(self):
+        if len(listOfertas) >= 2:
+            for i in listOfertas:
+                print("Oferta : ")
+                print(i)
+                socketServer.send_pyobj(i)
+                print(socketServer.recv_string())
+            
+    def run(self): #Metodo que se ejecutara con la llamada start
+          self.semaforo.acquire()
+          self.enviarOFertas()
+          self.semaforo.release()
+
+def recibirOferta():
+    while True:
+        print("esperando Oferta")
+        ofer = socketSub.recv_pyobj()
+        HiloEmpleador(semaforo,ofer).start()
+        HiloServidorEnviar(semaforo).start()
+    #end while
+#end def
+
+def hola():
+    while True:
+        print("hola")
+        time.sleep(2)
+
+hiloOfertas =  Thread(target=recibirOferta)
+
+hiloHola = Thread(target=hola)
+
+hiloOfertas.start()
+#hiloHola.start()
+
+
+# HiloEmpleador(semaforo,"x").start()
+# HiloEmpleador(semaforo,"x").start()
--- Codigo/servidor.py
+++ Codigo/servidor.py
@@ -1,15 +1,56 @@
 import zmq
+from Clases import  *
+from threading import Semaphore, Thread
+import sqlite3
+
+
+
+# conection.execute('''CREATE TABLE Ofertas
+#                (ID text,IDemple text, titulo text, descripcion text, experiencia text, estudio text,
+#                habilidades text, sector text)''')
+
+
 port = "6000"
 host = "25.86.45.96"
 #Creamos el contexto 
 context = zmq.Context()
+DHT = {}
+socketfilter = context.socket(zmq.REP)
+socketfilter.bind("tcp://{}:{}".format(host,port))
 
-socketServer = context.socket(zmq.REP)
-socketServer.bind("tcp://{}:{}".format(host,port))
 
-while True:
-    ofer = socketServer.recv_pyobj()
-    print(ofer)
-    #Guardar en la Base de datos 
+#semaforo encargado para poder hacer uso de la lista de Ofertas enviadas por los Empleadores
+semaforo = Semaphore(1)
 
-    socketServer.send("recibido")
+valorHash = 0
+class HiloAlamcenarenDHT(Thread):
+    # este hilo se encarga de agregar cada oferta a 
+    # una lista de ofertas
+    def __init__(self,semaforo,oferta): #Constructor de la clase
+         Thread.__init__(self)
+         self.semaforo = semaforo
+         self.oferta = oferta
+    def alamcenarOfertas(self):
+        
+        conection = sqlite3.connect('dataBase.db')
+        DHT[str(self.oferta.ID)] = self.oferta
+        conection.execute("""INSERT INTO Ofertas VALUES (
+            {},{},{},{},{},{},{},{})""".format(self.oferta.ID, self.oferta.idEmple,self.oferta.titulo,
+            self.oferta.descripcion,self.oferta.experiencia,self.oferta.estudio,self.oferta.habilidades,self.oferta.sector))
+    def run(self): #Metodo que se ejecutara con la llamada start
+          self.semaforo.acquire()
+          self.alamcenarOfertas()
+          self.semaforo.release()
+    
+def insertarOfertas():
+    while True:
+        obj = socketfilter.recv_pyobj()
+        print(valorHash)
+        obj.ID = valorHash
+        socketfilter.send_string("Check")
+        HiloAlamcenarenDHT(semaforo,obj).start()
+        valorHash += 1
+        pass
+
+hiloFiltro = Thread(target=insertarOfertas)
+hiloFiltro.start()
